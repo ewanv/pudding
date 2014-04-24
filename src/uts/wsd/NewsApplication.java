b68be6b63dcc752ff335c20996e586a4fb8affea
Ewan Vaughan <ewan.vaughan@gmail.com>
2014-04-24 19:19:14 +1000

    Add NewsApplication bean

---
+++ src/uts/wsd/NewsApplication.java
@@ -0,0 +1,19 @@
+package uts.wsd;
+
+public class NewsApplication {
+	private Articles articles;
+	private Authors authors;
+	
+	public Articles getArticles() {
+		return articles;
+	}
+	public void setArticles(Articles articles) {
+		this.articles = articles;
+	}
+	public Authors getAuthors() {
+		return authors;
+	}
+	public void setAuthors(Authors authors) {
+		this.authors = authors;
+	}
+}
